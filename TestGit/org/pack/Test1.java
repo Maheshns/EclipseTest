0e19659772d6a8dfff1d1b3637dbc97a38bdf2e0
Mahesh Prasad N.S <ns.maheshprasad@gmail.com>
2016-06-05 18:10:03 +0530

    Initial base

---
+++ TestGit/org/pack/Test1.java
@@ -0,0 +1,14 @@
+package org.pack;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello in main");
+		System.out.println("Hello in maindj dkjasldjlas");
+		
+
+	}
+
+}
